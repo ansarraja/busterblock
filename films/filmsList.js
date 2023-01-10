e29adc988c7e505031b56a12b35c61e4b7b542f5
Ansar Mehmood <ansar.mehmood@directlinegroup.co.uk>
2023-01-10 11:16:50 +0000

    changes made to filmsList.js - logic to show films list based on genre few changges  to  index.html and films.css

--- films/filmsList.js
+++ films/filmsList.js
@@ -2,44 +2,53 @@ var animationFilms = ["Toy Story", "Incredibles", "Up", "star wars clone wars", 
 
 var comedyFilms = ["Happy Gilmore", "White Chicks", "Hear no evil, speak no evil", "Life of Brian", "Team America: World Police", "Borat", "Dumb and Dumber", "Zoolander", "Coming to America", "Austin Powers"];
 
-var actionFilms =["The Northman, Wakanda Forever, The Batman, Thor, Iron Man 2, Death Race 2, The Spy Next Door, Shank, 13 Assassins, The King of Fighters "]
+var actionFilms = ["The Northman", "Wakanda Forever", "The Batman", "Thor", "Iron Man 2", "Death Race 2", "The Spy Next Door", "Shank", "13 Assassins", "The King of Fighters"]
 
-var horrorFilms =["The Evil Dead, The Exorcist, The Changeling, The Sixth Sense, Psycho, The Texas Chain Saw Massarce, Pieces, Saw, It, Screem "]
+var horrorFilms = ["The Evil Dead", "The Exorcist", "The Changeling", "The Sixth Sense", "Psycho", "The Texas Chain Saw Massarce", "Pieces", "Saw", "It", "Screem"]
 
 $("#film-category").on("click", ".btn", function (event) {
- 
+
  $("#film-list").empty();
  // get input
  var filmGenre = event.target.getAttribute('data-name');
- 
- if (filmGenre === "horror") {
-  seacrhCriteria = animationFilms;
-  divEl = seacrhCriteria;
 
-  $("#list-1").append(divEl);
+ if (filmGenre === "horror") {
+
+  seacrhCriteria = animationFilms;
+  for (var i = 0; i < seacrhCriteria.length; i++) {
+   var li = document.createElement("li");
+   li.textContent = seacrhCriteria[i];
+   $("#list-1").append(li);
+  }
  };
 
  if (filmGenre === "comedy") {
   seacrhCriteria = comedyFilms;
-  divEl = seacrhCriteria;
-
-  $("#list-2").append(divEl);
+  for (var i = 0; i < seacrhCriteria.length; i++) {
+   var li = document.createElement("li");
+   li.textContent = seacrhCriteria[i];
+   $("#list-2").append(li);
+  }
  }
 
 
  if (filmGenre === "action") {
-  seacrhCriteria = animationFilms;
-  divEl = seacrhCriteria;
-
-  $("#list-3").append(divEl);
+  seacrhCriteria = actionFilms;
+  for (var i = 0; i < seacrhCriteria.length; i++) {
+   var li = document.createElement("li");
+   li.textContent = seacrhCriteria[i];
+   $("#list-3").append(li);
+  }
  };
 
  if (filmGenre === "animation") {
   seacrhCriteria = animationFilms;
-  divEl = seacrhCriteria;
-
-  $("#list-4").append(divEl);
+  for (var i = 0; i < seacrhCriteria.length; i++) {
+   var li = document.createElement("li");
+   li.textContent = seacrhCriteria[i];
+   $("#list-4").append(li);
+  }
  };
 
- 
+
 })
